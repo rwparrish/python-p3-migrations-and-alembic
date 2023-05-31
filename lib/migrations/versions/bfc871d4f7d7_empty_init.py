ac85525c0e7bcdfa8f80e3e9fec1aa77a0372e1b
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-05-30 20:15:51 -0600

    MIGRATIONS

---
+++ lib/migrations/versions/bfc871d4f7d7_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: bfc871d4f7d7
+Revises: 
+Create Date: 2023-05-30 20:02:20.952381
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bfc871d4f7d7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
